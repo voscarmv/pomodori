b44080ade253aa5555b3afa37218555a4e45f429
oscar <oscar@localhost.localdomain>
2015-09-18 22:34:22 -0500

    Fixed control panel

--- control.php
+++ control.php
@@ -11,6 +11,29 @@ if(isset($_SESSION["valid_user"])){
         $username = $_SESSION["valid_user"];
 ?>
                 <p><a href="log_out.php">Log out</a></p>
+<?php
+        $conn = new mysqli('localhost', 'pomodori_user', 'tomatoes', 'pomodori');
+
+        if(mysqli_connect_error()){
+        ?>
+                <p>Could not connect to database</p>
+                <p><?php echo(mysqli_connect_error()); ?></p>
+        <?php
+        } else {
+        ?>
+                <p>Connection with database successful</p>
+        <?php
+                $result = $conn->query("select * from projects where username='$username'");
+
+                if(!$result){
+                ?>
+                <p>Could not query <b><?php echo($username) ?></b>'s projects.</p>
+                <p><?php echo($conn->error); ?></p>
+                <p><a href="index.html">Go back home</a></p>
+                <?php
+                } else {
+                ?>
+                <p>Query successful.</p>
                 <h2>Create a new project</h2>
                 <form action="new_project.php" method="post">
                         <p><label>Project title: <input type="text" name="title"></label></p>
@@ -19,51 +42,27 @@ if(isset($_SESSION["valid_user"])){
                 </form>
                 <h2>Current projects</h2>
                 <p><b><?php echo($username) ?></b>'s projects:</p>
-<?php
-        $conn = new mysqli('localhost', 'pomodori_user', 'tomatoes', 'pomodori');
-
-        if(mysqli_connect_error()){
-        ?>
-                        <p>Could not connect to database</p>
-                        <p><?php echo(mysqli_connect_error()); ?></p>
-        <?php
-        } else {
-        ?>
-                        <p>Connection with database successful</p>
-        <?php
-        }
-
-        $result = $conn->query("select * from projects where username='$username'");
-
-        if(!$result){
-        ?>
-                        <p>Could not query <b><?php echo($username) ?></b>'s projects.</p>
-                        <p><?php echo($conn->error); ?></p>
-                        <p><a href="index.html">Go back home</a></p>
-        <?php
-        } else {
-        ?>
-                        <p>Query successful.</p>
-        <?php
-                if($result->num_rows > 0){
-                        while ($row = mysqli_fetch_row($result)) {
-                                echo("<p> | ");
-                                foreach($row as $key => $value){
-                                        echo($value);
-                                        echo(" | ");
-                                }
-                                echo("</p>\n");
-                        }
-                } else {
-                ?>
-                        <p>There are no projects.</p>
                 <?php
+                        if($result->num_rows > 0){
+                                while ($row = mysqli_fetch_row($result)) {
+                                        echo("<p> | ");
+                                        foreach($row as $key => $value){
+                                                echo($value);
+                                                echo(" | ");
+                                        }
+                                        echo("</p>\n");
+                                }
+                        } else {
+                        ?>
+                <p>There are no projects.</p>
+                        <?php
+                        }
                 }
         }
 } else {
 ?>
-                        <p>You are not logged in.</p>
-                        <p><a href="index.html">Go back home</a></p>
+                <p>You are not logged in.</p>
+                <p><a href="index.html">Go back home</a></p>
 <?php
 }
 ?>
